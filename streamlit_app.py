de78c0ac07439ccc50cb38d0385d033e94561b9b
tazkyya <tazkyaoksana0410@gmail.com>
2025-05-15 14:07:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Mynewapp")
+st.title("🎈 My newapp")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
